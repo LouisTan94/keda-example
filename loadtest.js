58dec75f68b61e38aa1f8c523fc5840a2f70c4cb
kaisian <kaisian.tan@stengg.com>
2024-08-20 15:13:53 +0800

    rebuild

--- loadtest.js
+++ loadtest.js
@@ -2,11 +2,12 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 10,  // Number of virtual users
-  duration: '30s',  // Test duration
+  vus: 10,              // Number of Virtual Users
+  duration: '30s',     // Duration of the test
 };
 
 export default function () {
-  http.get('http://web-app-service');  // Replace with your service URL
-  sleep(1);
+  let res = http.get('http://nginx-metrics.default.svc.cluster.local:9113/metrics');
+  console.log(`Status: ${res.status}`);
+  sleep(1);             // Wait for 1 second between requests
 }
